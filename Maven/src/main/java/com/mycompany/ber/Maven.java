89cc680464845a31720eb358752ff0e079ea995f
jebin-ji <jebin.502223@sxcce.edu.in>
2025-02-17 13:25:59 +0530

    Add files via upload

---
+++ Maven/src/main/java/com/mycompany/ber/Maven.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ber;
+
+import java.util.Scanner;
+public class Maven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt(); 
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt(); 
+        int sum = num1 + num2; 
+        System.out.println("The sum is: " + sum); 
+    }
+}
